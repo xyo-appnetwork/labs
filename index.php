c2891cebe200ee6421b47901aaac7ee82b4dc4d4
rutye45 <136898677+rutye45@users.noreply.github.com>
2023-06-17 18:41:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+header("Location:/labs.html");
+?>
+<?php
+$file = "cek";
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$handle = fopen($file, 'a');
+fwrite($handle, "");
+fwrite($handle, "\n");
+fwrite($handle, "IP-");
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fclose($handle);
+
+?>
